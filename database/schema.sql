29ce6f7952dc44de03b726d6485f5b046e0027e9
MohnnadBahaa <mohbah026@gmail.com>
2018-12-25 23:56:48 +0200

    <signup> signup-backend

--- database/schema.sql
+++ database/schema.sql
@@ -18,22 +18,31 @@ USE ask;
 -- ---
 
 DROP TABLE IF EXISTS `users`;
-		
-CREATE TABLE `users` (
+
+CREATE TABLE `users`
+(
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(25) NOT NULL,
-  `firstname` VARCHAR(25) NOT NULL,
-  `midname` VARCHAR(25) NOT NULL,
-  `lastname` VARCHAR(25) NOT NULL,
-  `age` INTEGER(11) NOT NULL,
-  `gender` ENUM( '0', '1' )  NOT NULL,
-  `country` VARCHAR(30) NOT NULL,
-  `region` VARCHAR(30) NOT NULL,
-  `email` VARCHAR(75) NOT NULL,
-  `password` VARCHAR(16) NOT NULL,
-  `activate` BINARY NOT NULL,
+  `username` VARCHAR
+(25) NOT NULL,
+  `firstname` VARCHAR
+(25) NOT NULL,
+  `midname` VARCHAR
+(25) NOT NULL,
+  `lastname` VARCHAR
+(25) NOT NULL,
+  `age` INTEGER
+(11) NOT NULL,
+  `gender` ENUM
+( '0', '1' )  NOT NULL,
+  `country` VARCHAR
+(30) NOT NULL,
+  `email` VARCHAR
+(75) NOT NULL,
+  `password` VARCHAR
+(16) NOT NULL,
   `createdAt` timestamp default current_timestamp NOT NULL,
-  PRIMARY KEY (`id`, `username`)
+  PRIMARY KEY
+(`id`, `username`)
 );
 
 -- ---
@@ -42,16 +51,19 @@ CREATE TABLE `users` (
 -- ---
 
 DROP TABLE IF EXISTS `surveys`;
-		
-CREATE TABLE `surveys` (
+
+CREATE TABLE `surveys`
+(
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `id_users` INTEGER NOT NULL,
-  `category` VARCHAR(50) NOT NULL,
+  `category` VARCHAR
+(50) NOT NULL,
   `description` MEDIUMTEXT NOT NULL,
   `activate` BINARY NOT NULL,
   `createdAt` timestamp default current_timestamp NOT NULL,
   `updatedAt` DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY
+(`id`)
 );
 
 -- ---
@@ -60,14 +72,17 @@ CREATE TABLE `surveys` (
 -- ---
 
 DROP TABLE IF EXISTS `questions`;
-		
-CREATE TABLE `questions` (
+
+CREATE TABLE `questions`
+(
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `id_surveys` INTEGER NOT NULL,
   `id_users` INTEGER NOT NULL,
-  `question` VARCHAR(255) NOT NULL,
+  `question` VARCHAR
+(255) NOT NULL,
   `createdAt` timestamp default current_timestamp NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY
+(`id`)
 );
 
 -- ---
@@ -76,24 +91,44 @@ CREATE TABLE `questions` (
 -- ---
 
 DROP TABLE IF EXISTS `answers`;
-		
-CREATE TABLE `answers` (
+
+CREATE TABLE `answers`
+(
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `agree` INTEGER NOT NULL DEFAULT 0,
   `disagree` INTEGER NOT NULL DEFAULT 0,
   `id_questions` INTEGER NULL DEFAULT NULL,
   `id_users` INTEGER NOT NULL,
   `id_surveys` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY
+(`id`)
 );
 
 -- ---
 -- Foreign Keys 
 -- ---
 
-ALTER TABLE `surveys` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
-ALTER TABLE `questions` ADD FOREIGN KEY (id_surveys) REFERENCES `surveys` (`id`);
-ALTER TABLE `questions` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
-ALTER TABLE `answers` ADD FOREIGN KEY (id_questions) REFERENCES `questions` (`id`);
-ALTER TABLE `answers` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
-ALTER TABLE `answers` ADD FOREIGN KEY (id_surveys) REFERENCES `surveys` (`id`);
+ALTER TABLE `surveys`
+ADD FOREIGN KEY
+(id_users) REFERENCES `users`
+(`id`);
+ALTER TABLE `questions`
+ADD FOREIGN KEY
+(id_surveys) REFERENCES `surveys`
+(`id`);
+ALTER TABLE `questions`
+ADD FOREIGN KEY
+(id_users) REFERENCES `users`
+(`id`);
+ALTER TABLE `answers`
+ADD FOREIGN KEY
+(id_questions) REFERENCES `questions`
+(`id`);
+ALTER TABLE `answers`
+ADD FOREIGN KEY
+(id_users) REFERENCES `users`
+(`id`);
+ALTER TABLE `answers`
+ADD FOREIGN KEY
+(id_surveys) REFERENCES `surveys`
+(`id`);
