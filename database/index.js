6291500832a7f18ae0f8e4d1cddd03727723334d
IssaAnagreh <isa_anagreh@hotmail.com>
2018-12-26 23:35:44 +0200

    <enhancement> add database helper function

--- database/index.js
+++ database/index.js
@@ -11,4 +11,27 @@ const selectAll = function(tableName, callback) {
   });
 };
 
+const selectAllSurveys = function(userID, callback) {
+  dbconnection.query(`SELECT * FROM surveys where id_users = ${userID}`, function(err, results) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+const selectAllAnsweredSurveys = function(userID, callback) {
+  dbconnection.query(`SELECT * FROM answers where id_users = ${userID}`, function(err, results) {
+    if (err) {
+      callback(err, null);
+    } else {
+      dbconnection.query(`SELECT * FROM surveys where id = ${results.id_surveys}`, function(err, results) {
+        callback(null, results);
+      })
+    }
+  })
+};
+
 module.exports.selectAll = selectAll;
+module.exports.selectAllSurveys = selectAllSurveys;
