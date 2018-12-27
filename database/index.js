b3ad41e1b691cd2b53eb04c6087e73573838187d
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-12-27 15:25:19 +0200

    <enhancement> add database helper for survey

--- database/index.js
+++ database/index.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 const dbconnection = require("./config.js");
 
-const selectAll = function(tableName, callback) {
-    dbconnection.query(`SELECT * FROM ${tableName}`, function(err, results) {
+const selectAll = (tableName, callback) => {
+  dbconnection.query(`SELECT * FROM ${tableName}`, (err, results) => {
     if (err) {
       callback(err, null);
     } else {
@@ -11,26 +11,55 @@ const selectAll = function(tableName, callback) {
   });
 };
 
-const selectAllSurveys = function(userID, callback) {
-  dbconnection.query(`SELECT * FROM surveys where id_users = ${userID}`, function(err, results) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
-
-const selectAllAnsweredSurveys = function(userID, callback) {
-  dbconnection.query(`SELECT * FROM answers where id_users = ${userID}`, function(err, results) {
-    if (err) {
-      callback(err, null);
-    } else {
-      dbconnection.query(`SELECT * FROM surveys where id = ${results.id_surveys}`, function(err, results) {
+const selectAllSurveys = (userID, callback) => {
+  dbconnection.query(
+    `SELECT * FROM surveys where id_users = ${userID}`,
+    (err, results) => {
+      if (err) {
+        callback(err, null);
+      } else {
         callback(null, results);
-      })
+      }
     }
-  })
+  );
+};
+
+const selectAllAnsweredSurveys = (userID, callback) => {
+  dbconnection.query(
+    `SELECT * FROM answers where id_users = ${userID}`,
+    (err, results) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        dbconnection.query(
+          `SELECT * FROM surveys where id = ${results.id_surveys}`,
+          function(err, results) {
+            callback(null, results);
+          }
+        );
+      }
+    }
+  );
+};
+
+//enhance query to save for a specific user
+const insertSurvey = (
+  surveyName,
+  surveyCategory,
+  surveyDescription,
+  callback
+) => {
+  callback(null, { success: "done inserting survey!" });
+  // dbconnection.query(
+  //   `INSERT INTO SURVEYS(survey_name, category, description) VALUES(\"${surveyName}\",\"${surveyCategory}\",\"${surveyDescription}\")`,
+  //   (err, result) => {
+  //     if (err) {
+  //       callback(err, null);
+  //     } else {
+  //       callback(null, result);
+  //     }
+  //   }
+  // );
 };
 
 /*
@@ -40,3 +69,4 @@ get answers
 
 module.exports.selectAll = selectAll;
 module.exports.selectAllSurveys = selectAllSurveys;
+module.exports.insertSurvey = insertSurvey;
