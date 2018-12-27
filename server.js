751a3e61536c8cc8341f92dabd36fec7e96f1f2d
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-12-27 02:43:26 +0200

    <enhancement> add survey server helpers

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-var express = require("express");
-// var router = express.Router();
+const express = require("express");
 // const cors = require('cors');
-var bodyParser = require("body-parser");
-var brain = require("./server/brain.js");
-var db = require("./database/index");
+const bodyParser = require("body-parser");
+const brain = require("./server/brain.js");
+const db = require("./database/index");
+const surveyHelpers = require("./server/surveyHelpers");
 
-var app = express();
+const app = express();
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
 
 app.get("/isa", function(req, res) {
@@ -21,10 +19,9 @@ app.post("/search", function(req, res) {
   res.send({});
 });
 
-app.post("/surveys", function(req, res) {
-  console.log("search server", req.body);
-  res.send({});
-});
+app.get("/surveys", surveyHelpers.selectAllSurveys);
+
+app.post("/surveys", surveyHelpers.insertSurvey);
 
 app.post("/mysurveys", function(req, res) {
   console.log("search server", req.body);
@@ -37,17 +34,15 @@ app.get("/user", function(req, res) {
 });
 
 app.post("/signup", function(req, res) {
-  var firstName = req.body.firstName;
-  var midname = req.body.midname;
-  var lastName = req.body.lastName;
-  var age = req.body.age;
-  var gender = req.body.gender;
-  var country = req.body.country;
-  var email = req.body.email;
-  var username = req.body.username;
-  var password = req.body.password;
-
-  bcrypt.hash(password, 12);
+  let firstName = req.body.firstName;
+  let midname = req.body.midname;
+  let lastName = req.body.lastName;
+  let age = req.body.age;
+  let gender = req.body.gender;
+  let country = req.body.country;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
 
   bcrypt.hash(password, 12).then(function(hashedPassword) {
     console.log("hashed password", hashedPassword);
@@ -63,28 +58,28 @@ app.post("/signup", function(req, res) {
   });
 });
 
-app.post("login",function(req,res)){
+// app.post("login",function(req,res)){
 
-  // var username = req.body.username;
-  // var password = req.body.password;
+//   // var username = req.body.username;
+//   // var password = req.body.password;
 
-  // var query = `select * from users where username=\"${username}\"`;
-  // // 0 -> no username 
-  // db.dbConnection.query(query,function(err,result){
-  //   if(result){
-  //       // make compare 
-  //       // //  bcrypt.compare(password, result[0].password, function(err, data) {
-  //         if (data) {
-  //       //     var userId = result[0].id
-  //       //     var user = result[0]
-  //       //     var username=result[0].username
-  //       //     console.log('User info-->',username)
-  //   } else {
-  //     res.send('0');
-  //   }
-  // })
+//   // var query = `select * from users where username=\"${username}\"`;
+//   // // 0 -> no username
+//   // db.dbConnection.query(query,function(err,result){
+//   //   if(result){
+//   //       // make compare
+//   //       // //  bcrypt.compare(password, result[0].password, function(err, data) {
+//   //         if (data) {
+//   //       //     var userId = result[0].id
+//   //       //     var user = result[0]
+//   //       //     var username=result[0].username
+//   //       //     console.log('User info-->',username)
+//   //   } else {
+//   //     res.send('0');
+//   //   }
+//   // })
 
-}
+// }
 
 //connection for everything except for Brain
 app.listen(3000, function() {
