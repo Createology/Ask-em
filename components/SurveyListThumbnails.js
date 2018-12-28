0cc448bc56d973cbf5f891ff773980f61f15d438
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-12-28 21:39:56 +0200

    <feature> add thumnails survey list component

---
+++ components/SurveyListThumbnails.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { TouchableHighlight, View } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button
+} from "native-base";
+export default class SurveyListThumbnails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allSurveysInfo: props.allSurveys,
+      surveyImages: props.surveyImages
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        {Array.isArray(this.props.allSurveys) &&
+          this.props.allSurveys.map((survey, index) => (
+            <List key={survey.id}>
+              <ListItem thumbnail>
+                <Left>
+                  <Thumbnail
+                    square
+                    source={{
+                      uri: this.state.surveyImages[index]
+                    }}
+                  />
+                </Left>
+                <Body>
+                  <TouchableHighlight
+                    onPress={() => {
+                      this.props.selectedSurvey(survey.survey_name);
+                      this.props.showHandler();
+                    }}
+                    underlayColor="gray"
+                  >
+                    <Text key={survey.id}>{survey.survey_name}</Text>
+                  </TouchableHighlight>
+
+                  <Text note numberOfLines={3}>
+                    {survey.description}
+                  </Text>
+                </Body>
+                <Right>
+                  <TouchableHighlight
+                    onPress={() => {
+                      this.props.selectedSurvey(survey.survey_name);
+                      this.props.showHandler();
+                    }}
+                    underlayColor="gray"
+                  >
+                    <Text key={survey.id} style={{ color: "blue" }}>
+                      View
+                    </Text>
+                  </TouchableHighlight>
+                </Right>
+              </ListItem>
+            </List>
+          ))}
+      </Container>
+    );
+  }
+}
