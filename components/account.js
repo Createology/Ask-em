5e84a2cf5d49cd33b540fe9482daa0cd25e8c814
MohnnadBahaa <mohbah026@gmail.com>
2019-01-02 06:19:32 +0200

    <feat> user Account-frontEnd-MySuerveysHaveAnswerd-styels

--- components/account.js
+++ components/account.js
@@ -101,3 +101,131 @@ export default class Account extends Component {
       console.warn("error from the token surveysAnsByUser", error);
     }
   };
+
+  setModalVisible = visible => {
+    this.setState({ modalVisible: visible });
+  };
+
+  selectedSurvey = item => {
+    this.setState({ selectedSurvey: item });
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <Grid>
+          <Row size={2}>
+            <View style={styles.container}>
+              <View style={styles.header} />
+              <Image
+                style={styles.avatar}
+                source={{
+                  uri: "https://bootdey.com/img/Content/avatar/avatar6.png"
+                }}
+              />
+              <View style={styles.body}>
+                <View style={styles.bodyContent}>
+                  <Text style={styles.name}>John Doe</Text>
+                  <Text style={styles.info}>
+                    UX Designer / Mobile developer
+                  </Text>
+                  <Text style={styles.description}>
+                    Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne
+                    assum electram expetendis, omittam deseruisse consequuntur
+                    ius an,
+                  </Text>
+                  <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => {
+                      this.onPressMySurveys();
+                    }}
+                  >
+                    <Text>My Surveys</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => {
+                      this.onPressSurveysHasBeenAns();
+                    }}
+                  >
+                    <Text>Answered Surveys</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Row>
+          <Row size={2}>
+            <SurveyListThumbnails
+              allSurveys={this.state.fetchedSurveys}
+              selectedSurvey={this.selectedSurvey.bind(this)}
+              showHandler={this.setModalVisible.bind(this)}
+              surveyImages={this.state.images}
+            />
+          </Row>
+        </Grid>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#00BFFF",
+    height: 200
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 75
+  },
+  name: {
+    fontSize: 22,
+    color: "#FFFFFF",
+    fontWeight: "600"
+  },
+  body: {
+    marginTop: 40
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: "center",
+    padding: 30
+  },
+  name: {
+    fontSize: 28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info: {
+    fontSize: 16,
+    color: "#00BFFF",
+    marginTop: 10
+  },
+  description: {
+    fontSize: 16,
+    color: "#696969",
+    marginTop: 10,
+    textAlign: "center"
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#00BFFF"
+  },
+  thumbnails: {
+    // marginTop: 50,
+    color: "black"
+  }
+});
