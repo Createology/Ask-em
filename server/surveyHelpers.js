4d2f38c819a9a889c34f0dbabbddc91c616c86b3
MohnnadBahaa <mohbah026@gmail.com>
2019-01-03 17:33:55 +0200

    <refactor> Home/getAllsurveys

--- server/surveyHelpers.js
+++ server/surveyHelpers.js
@@ -28,7 +28,7 @@ getAllSurveysOfUser = (req, res) => {
 };
 
 getAllSurveys = (req, res) => {
-  DB.selectAll("surveys", (err, result) => {
+  DB.selectAllActiveSuerveyNotAnswerd(req.body.id, (err, result) => {
     if (result) {
       res.status(200).send(result);
     } else {
@@ -40,41 +40,51 @@ getAllSurveys = (req, res) => {
 const fillSmartAnswer = (req, res) => {
   console.log("===fillSmartAnswer===");
   if (req.body.answer) {
-    const { answer, questionID, userID, surveyID} = req.body;
-    console.log(result)
-    DB.insertSmartAnswer(answer, questionID, userID, surveyID, (err, results) => {
-      if (err) {res.sendStatus(404)} else {
-        if (results.length > 0) {
-          res.status(200).send(results)
+    const { answer, questionID, userID, surveyID } = req.body;
+    console.log(result);
+    DB.insertSmartAnswer(
+      answer,
+      questionID,
+      userID,
+      surveyID,
+      (err, results) => {
+        if (err) {
+          res.sendStatus(404);
         } else {
-          res.status(401).send("no results")
+          if (results.length > 0) {
+            res.status(200).send(results);
+          } else {
+            res.status(401).send("no results");
+          }
         }
       }
-    })
+    );
   } else {
-    res.status(402).send("no answer")
+    res.status(402).send("no answer");
   }
-  res.sendStatus(500)
+  res.sendStatus(500);
 };
 
 const fillAnswer = (req, res) => {
   console.log("===fillAnswer===");
   if (req.body.answer) {
-    const { answer, questionID, userID, surveyID} = req.body;
-    console.log(result)
+    const { answer, questionID, userID, surveyID } = req.body;
+    console.log(result);
     DB.insertAnswer(answer, questionID, userID, surveyID, (err, results) => {
-      if (err) {res.sendStatus(404)} else {
+      if (err) {
+        res.sendStatus(404);
+      } else {
         if (results.length > 0) {
-          res.status(200).send(results)
+          res.status(200).send(results);
         } else {
-          res.status(401).send("no results")
+          res.status(401).send("no results");
         }
       }
-    })
+    });
   } else {
-    res.status(402).send("no answer")
+    res.status(402).send("no answer");
   }
-  res.sendStatus(500)
+  res.sendStatus(500);
 };
 
 module.exports.saveSurvey = saveSurvey;
