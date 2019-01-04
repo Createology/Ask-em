f2332b69a2ff18d143a01cedfa81958e3e90edbb
omar-dulaimi <o.m.dulaimi@gmail.com>
2019-01-04 15:21:39 +0200

    <refactor> change home component to work with refactored server routes

--- components/home.js
+++ components/home.js
@@ -7,20 +7,26 @@ import {
   StatusBar,
   KeyboardAvoidingView,
   TouchableHighlight,
-  AsyncStorage,
+  AsyncStorage
 } from "react-native";
-import { Container, Header, Text as Textbase, Left, Icon } from "native-base";
+import {
+  Container,
+  Header,
+  Text as Textbase,
+  Left,
+  Icon,
+  Button
+} from "native-base";
 import SurveyModal from "./SurveyModal";
 import SurveyListThumbnails from "./SurveyListThumbnails";
-const ip = require("../ip.json");
+import SurveyStats from "./SurveyStats";
 
+const ip = require("../ip.json");
 
 export default class Home extends Component {
   static navigationOptions = {
     title: "Home",
-    drawerIcon: () => (
-      <Icon name='home' style={{ fontSize: 40 }} />
-    )
+    drawerIcon: () => <Icon name="home" style={{ fontSize: 40 }} />
   };
 
   constructor(props) {
@@ -49,8 +55,14 @@ export default class Home extends Component {
       surveyName: "",
       surveyDescription: "",
       surveyCategory: "",
+      selectedEducation: "",
+      selectedMarital: "",
+      surveyUserID: "",
+      surveyID: "",
       loggedin: "",
       allSurveysInfo: [],
+      surveyAnswers: [],
+      userID: null,
       images: [
         "https://cdn-images-1.medium.com/max/1200/1*jh6bmapyE8nPWju7W_7qEw.png",
         "https://softwareengineeringdaily.com/wp-content/uploads/2018/12/machinelearning.jpg",
@@ -105,29 +117,80 @@ export default class Home extends Component {
     });
   };
 
-  onPressSubmitModal(surveyName, surveyDescription, surveyCategory) {
-    [...arguments].forEach(element => {
-      this.setState({ element });
-    });
-
-    fetch(`${ip}:3000/surveys`, {
+  getQuestions(surveyID) {
+    fetch(`${ip}:3000/answer/dumb/questions`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        surveyName: surveyName,
-        surveyDescription: surveyDescription,
-        surveyCategory: surveyCategory
+        surveyID: surveyID
       })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(res => {
+        // console.warn(res);
+      })
+      .done();
+  }
+
+  async onPressSubmitModal() {
+    const surveyAndUserIDs = [this.state.surveyID, this.state.surveyUserID];
+    const args = [...arguments];
+    //30 => education level
+    //37 => marital status
+    const questionsIDs = [30, 37];
+    const answersArray = [];
+
+    args.forEach((item, index) => {
+      answersArray.push([
+        item,
+        questionsIDs[index],
+        surveyAndUserIDs[0],
+        surveyAndUserIDs[1]
+      ]);
+    });
+
+    await this.setState({ surveyAnswers: answersArray });
+    // console.warn(this.state.surveyAnswers);
+    fetch(`${ip}:3000/answer/dumb/add`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ answers: this.state.surveyAnswers })
     })
       .then(response => response.json())
       .then(res => {
+        // console.warn(res);
         this.setState({ modalVisible: false });
-        console.warn(res);
       });
   }
 
-  showAllSurveys = () => {
-    fetch(`${ip}:3000/surveys/`, {
+  async showAllSurveys() {
+    try {
+      const value = await AsyncStorage.getItem("userID");
+      if (value !== null) {
+        const token = JSON.parse(value);
+        this.setState({
+          userID: ` ${token.user_id} `
+        });
+        fetch(`${ip}:3000/surveys/retrieve`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ id: this.state.userID })
+        })
+          .then(response => response.json())
+          .then(res => {
+            this.setState({ allSurveysInfo: res });
+          })
+          .done();
+      }
+    } catch (error) {
+      console.warn("Error getting Token!", error);
+    }
+  }
+
+  getSurveysAnswers = () => {
+    fetch(`${ip}:3000/surveysanswers/`, {
       method: "GET"
     })
       .then(response => {
@@ -135,10 +198,26 @@ export default class Home extends Component {
       })
       .then(res => {
         this.setState({
-          allSurveysInfo: res
+          surveyAnswers: res
         });
       })
       .done();
+  };
+
+  onChangeSurveyInfo = (
+    name,
+    description,
+    category,
+    surveyID,
+    surveyUserID
+  ) => {
+    this.setState({
+      surveyName: name,
+      surveyDescription: description,
+      surveyCategory: category,
+      surveyID: surveyID,
+      surveyUserID: surveyUserID
+    });
   };
 
   render() {
@@ -146,27 +225,39 @@ export default class Home extends Component {
       modalVisible,
       selectedSurvey,
       allSurveysInfo,
-      images
+      images,
+      surveyDescription,
+      surveyCategory,
+      surveyName,
+      loggedin,
+      surveyUserID,
+      surveyID
     } = this.state;
 
     const { navigation } = this.props;
-    if (navigation.getParam('accessToken')) {
-      if (navigation.getParam('accessToken').length > 0) {
-        var itemId = navigation.getParam('accessToken');
+    if (navigation.getParam("accessToken")) {
+      if (navigation.getParam("accessToken").length > 0) {
+        var itemId = navigation.getParam("accessToken");
       } else {
         var itemId = " ";
       }
     } else {
       var itemId = " ";
     }
-    if (this.state.loggedin.length > 1) {
-      var itemId = this.state.loggedin;
+    if (loggedin.length > 1) {
+      var itemId = loggedin;
     }
     return (
       <View>
-        <Header style={{backgroundColor: "#E65100"}}>
+        <Header style={{ backgroundColor: "#E65100" }}>
           <Left>
-            <Icon style={styles.icon} name='menu' onPress={() => { this.props.navigation.openDrawer() }} />
+            <Icon
+              style={styles.icon}
+              name="menu"
+              onPress={() => {
+                this.props.navigation.openDrawer();
+              }}
+            />
           </Left>
           <Text style={styles.headerStyle}>Welcome{itemId}to ASKem!</Text>
         </Header>
@@ -176,6 +267,10 @@ export default class Home extends Component {
               showHandler={this.setModalVisible.bind(this)}
               visibility={modalVisible}
               selectedSurvey={selectedSurvey}
+              allSurveys={allSurveysInfo}
+              surveyName={surveyName}
+              surveyDescription={surveyDescription}
+              surveyCategory={surveyCategory}
               submitModalHandler={this.onPressSubmitModal.bind(this)}
             />
             <SurveyListThumbnails
@@ -183,6 +278,8 @@ export default class Home extends Component {
               selectedSurvey={this.selectedSurvey.bind(this)}
               showHandler={this.setModalVisible.bind(this)}
               surveyImages={images}
+              onChangeSurveyInfo={this.onChangeSurveyInfo.bind(this)}
+              getQuestions={this.getQuestions.bind(this)}
             />
           </View>
         </ScrollView>
