b84a856b8512189f90a6c58cad9c1470998aa410
MohnnadBahaa <mohbah026@gmail.com>
2019-01-06 03:06:47 +0200

    <feat> add dummy table

--- database/schema.sql
+++ database/schema.sql
@@ -206,6 +206,39 @@ SET time_zone
 -- Table structure for table `answers`
 --
 
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 06, 2019 at 02:03 AM
+-- Server version: 8.0.13
+-- PHP Version: 7.2.10-0ubuntu0.18.04.1
+
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT
+= 0;
+START TRANSACTION;
+SET time_zone
+= "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `askem_77`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
 CREATE TABLE `answers`
 (
   `id` int
@@ -232,119 +265,46 @@ id`,
 `, `id_questions`, `id_users`, `id_surveys`) VALUES
 (93, 'primary', 30, 44, 5),
 (94, 'single', 37, 44, 5),
-(95, 'asd', 30, 44, 1),
-(96, 'primary', 30, 1, 1),
-(97, 'single', 37, 1, 1),
-(98, 'primary', 30, 2, 2),
-(99, 'single', 37, 2, 2),
-(100, 'primary', 30, 44, 5),
-(101, 'single', 37, 44, 5),
-(102, 'primary', 30, 44, 5),
-(103, 'single', 37, 44, 5),
-(104, 'primary', 30, 44, 5),
-(105, 'single', 37, 44, 5),
-(106, 'primary', 30, 1, 1),
-(107, 'single', 37, 1, 1),
-(108, 'primary', 30, 44, 5),
-(109, 'single', 37, 44, 5),
-(110, 'primary', 30, 2, 2),
-(111, 'single', 37, 2, 2),
-(112, 'primary', 30, 1, 1),
-(113, 'single', 37, 1, 1),
-(114, 'primary', 30, 44, 5),
-(115, 'single', 37, 44, 5),
-(116, 'primary', 30, 44, 5),
-(117, 'single', 37, 44, 5),
-(118, 'primary', 30, 2, 2),
-(119, 'single', 37, 2, 2),
-(121, 'high', 30, 2, 2),
-(122, 'single', 37, 2, 2),
-(123, 'doctoral', 30, 2, 2),
-(124, 'divorced', 37, 2, 2),
-(125, 'master', 30, 2, 2),
-(126, 'single', 37, 2, 2),
 (127, 'master', 30, 45, 6),
 (128, 'divorced', 37, 45, 6),
-(129, 'primary', 30, 45, 6),
-(130, 'single', 37, 45, 6),
-(131, 'primary', 30, 45, 6),
-(132, 'single', 37, 45, 6),
-(133, 'primary', 30, 2, 2),
-(134, 'single', 37, 2, 2),
-(135, 'primary', 30, 45, 6),
-(136, 'single', 37, 45, 6),
-(137, 'primary', 30, 2, 2),
-(138, 'single', 37, 2, 2),
-(139, 'primary', 30, 45, 6),
-(140, 'single', 37, 45, 6),
-(141, 'primary', 30, 45, 6),
-(142, 'single', 37, 45, 6),
-(143, 'primary', 30, 2, 2),
-(144, 'single', 37, 2, 2),
-(145, 'primary', 30, 45, 6),
-(146, 'single', 37, 45, 6),
-(147, 'primary', 30, 2, 2),
-(148, 'single', 37, 2, 2),
-(149, 'primary', 30, 2, 2),
-(150, 'single', 37, 2, 2),
-(151, 'primary', 30, 45, 6),
-(152, 'single', 37, 45, 6),
-(153, 'primary', 30, 2, 2),
-(154, 'single', 37, 2, 2),
-(155, 'primary', 30, 45, 6),
-(156, 'single', 37, 45, 6),
-(157, 'primary', 30, 2, 2),
-(158, 'single', 37, 2, 2),
-(159, 'primary', 30, 2, 2),
-(160, 'single', 37, 2, 2),
 (161, 'master', 30, 1, 1),
 (162, 'single', 37, 1, 1),
-(163, 'primary', 30, 45, 6),
-(164, 'single', 37, 45, 6),
-(165, 'primary', 30, 2, 2),
-(166, 'single', 37, 2, 2),
-(167, 'primary', 30, 45, 6),
-(168, 'single', 37, 45, 6),
-(169, 'primary', 30, 45, 6),
-(170, 'single', 37, 45, 6),
-(171, 'primary', 30, 2, 2),
-(172, 'single', 37, 2, 2),
-(173, 'primary', 30, 45, 6),
-(174, 'single', 37, 45, 6),
-(175, 'primary', 30, 2, 2),
-(176, 'single', 37, 2, 2),
-(177, 'primary', 30, 45, 6),
-(178, 'single', 37, 45, 6),
-(179, 'master', 30, 2, 2),
-(180, 'seperated', 37, 2, 2),
-(181, 'primary', 30, 45, 6),
-(182, 'single', 37, 45, 6),
-(183, 'master', 30, 2, 2),
-(184, 'married', 37, 2, 2),
-(185, 'primary', 30, 45, 6),
-(186, 'single', 37, 45, 6),
 (187, 'bachelor', 30, 44, 7),
 (188, 'seperated', 37, 44, 7),
-(189, 'primary', 30, 2, 2),
-(190, 'single', 37, 2, 2),
-(191, 'primary', 30, 45, 6),
-(192, 'single', 37, 45, 6),
-(193, 'primary', 30, 45, 6),
-(194, 'single', 37, 45, 6),
-(195, 'primary', 30, 2, 2),
-(196, 'single', 37, 2, 2),
-(197, 'primary', 30, 2, 2),
-(198, 'single', 37, 2, 2),
-(199, 'primary', 30, 2, 2),
-(200, 'single', 37, 2, 2),
-(201, 'primary', 30, 45, 6),
-(202, 'single', 37, 45, 6),
-(203, 'primary', 30, 2, 2),
-(204, 'single', 37, 2, 2),
-(205, 'primary', 30, 45, 6),
-(206, 'single', 37, 45, 6),
 (207, 'primary', 30, 2, 2),
-(208, 'single', 37, 2, 2);
+(208, 'single', 37, 2, 2),
+(209, 'primary', 30, 44, 10),
+(210, 'single', 37, 44, 10),
+(211, 'primary', 30, 44, 19),
+(212, 'single', 37, 44, 19),
+(213, 'primary', 30, 44, 15),
+(214, 'single', 37, 44, 15),
+(217, 'primary', 30, 44, 18),
+(218, 'single', 37, 44, 18),
+(225, 'primary', 30, 45, 26),
+(226, 'single', 37, 45, 26),
+(227, 'primary', 30, 44, 24),
+(228, 'single', 37, 44, 24),
+(229, 'bachelor', 30, 44, 27),
+(230, 'seperated', 37, 44, 27),
+(233, 'bachelor', 30, 44, 22),
+(234, 'seperated', 37, 44, 22),
+(239, 'primary', 30, 44, 28),
+(240, 'single', 37, 44, 28),
+(251, 'primary', 30, 44, 29),
+(252, 'single', 37, 44, 29),
+(257, 'primary', 30, 44, 25),
+(258, 'single', 37, 44, 25),
+(263, 'primary', 30, 70, 31),
+(264, 'single', 37, 70, 31),
+(265, 'primary', 30, 44, 30),
+(266, 'single', 37, 44, 30),
+(269, 'primary', 30, 70, 32),
+(270, 'single', 37, 70, 32),
+(279, 'primary', 30, 44, 23),
+(280, 'single', 37, 44, 23),
+(293, 'primary', 30, 45, 26),
+(294, 'single', 37, 45, 26);
 
 -- --------------------------------------------------------
 
@@ -398,7 +358,62 @@ id`,
 (2, 44, 'read', '999', 'kjhg'),
 (3, 44, 'read', '999', 'kjhg'),
 (4, 44, 'read', '3333', 'ggg'),
-(5, 44, 'read', 'gggg', '66666');
+(5, 44, 'read', 'gggg', '66666'),
+(6, 44, 'read', '2345', 'jh'),
+(7, 44, 'read', '0786927200', 'Ghadeer'),
+(8, 44, 'read', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dummy`
+--
+
+CREATE TABLE `dummy`
+(
+  `id` int
+(11) NOT NULL,
+  `answer` varchar
+(225) CHARACTER
+SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
+  `id_questions` int
+(11) DEFAULT NULL,
+  `id_users` int
+(11) NOT NULL,
+  `id_surveys` int
+(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `dummy`
+--
+
+INSERT INTO `dummy` (`
+id`,
+`answer
+`, `id_questions`, `id_users`, `id_surveys`) VALUES
+(2, 'Hashmi', 67, 70, 36),
+(3, 'Khalda', 67, 70, 36),
+(4, 'Kursy', 67, 70, 36),
+(5, 'Marka', 67, 70, 36),
+(6, 'Shmiesani', 67, 70, 36),
+(7, 'Tabarbour', 67, 70, 36),
+(8, '3', 68, 70, 36),
+(9, '4', 68, 70, 36),
+(10, '7', 68, 70, 36),
+(11, '2', 68, 70, 36),
+(12, '5', 68, 70, 36),
+(13, 'yes', 69, 70, 36),
+(14, 'no', 69, 70, 36),
+(15, 'yes', 69, 70, 36),
+(16, 'yes', 69, 70, 36),
+(17, 'no', 69, 70, 36),
+(18, 'yes', 70, 70, 36),
+(19, 'yes', 70, 70, 36),
+(20, 'no', 70, 70, 36),
+(21, 'yes', 70, 70, 36),
+(22, 'no', 70, 70, 36);
 
 -- --------------------------------------------------------
 
@@ -434,7 +449,14 @@ id`,
 (38, 6, 45, 'asdasdasdasdasdasdasd'),
 (39, 7, 44, 'Suitable place to build a new mall'),
 (40, 7, 44, 'Number of mall levels needed'),
-(41, 7, 44, 'Is a new mall at dabouq a good idea');
+(41, 7, 44, 'Is a new mall at dabouq a good idea'),
+(45, 19, 44, 're'),
+(65, 34, 70, 'Where is the suitable place to build a mall'),
+(66, 34, 70, 'How many levels mall should have'),
+(67, 36, 70, 'Suitabl place to build a mall in Amman'),
+(68, 36, 70, 'How many levels for this mall'),
+(69, 36, 70, 'Should the mall has a cinema'),
+(70, 36, 70, 'Would you prefer see an entertainment section in the mall');
 
 -- --------------------------------------------------------
 
@@ -450,6 +472,10 @@ CREATE TABLE `smart`
 (225) CHARACTER
 SET utf8mb4
 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
+  `Truth` enum
+('0','1') CHARACTER
+SET utf8mb4
+COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '1',
   `id_questions` int
 (11) DEFAULT NULL,
   `id_users` int
@@ -465,19 +491,19 @@ COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '0',
 INSERT INTO `smart` (`
 id`,
 `smartanswer
-`, `id_questions`, `id_users`, `id_surveys`) VALUES
-(1, 'Jandaweel', 39, 1, 7),
-(2, 'Khalda', 39, 2, 7),
-(3, 'Dabouq', 39, 6, 7),
-(4, '6', 40, 1, 7),
-(5, '5', 40, 2, 7),
-(6, '3', 40, 6, 7),
-(7, 'Yes', 41, 1, 7),
-(8, 'Yes', 41, 2, 7),
-(9, 'Yes', 41, 6, 7),
-(10, 'Tabarbour', 39, 9, 7),
-(11, '1', 40, 9, 7),
-(12, 'No', 41, 9, 7);
+`, `Truth`, `id_questions`, `id_users`, `id_surveys`) VALUES
+(1, 'Jandaweel', '1', 39, 1, 7),
+(2, 'Khalda', '1', 39, 2, 7),
+(3, 'Dabouq', '1', 39, 6, 7),
+(4, '6', '1', 40, 1, 7),
+(5, '5', '1', 40, 2, 7),
+(6, '3', '1', 40, 6, 7),
+(7, 'Yes', '1', 41, 1, 7),
+(8, 'Yes', '1', 41, 2, 7),
+(9, 'Yes', '1', 41, 6, 7),
+(10, 'Tabarbour', '1', 39, 9, 7),
+(11, '1', '1', 40, 9, 7),
+(12, 'No', '1', 41, 9, 7);
 
 -- --------------------------------------------------------
 
@@ -519,7 +545,34 @@ id`,
 (2, 2, 'smart car', '3', 'smart car navigation ', '1'),
 (5, 44, 'MAC usage in middle east', '2', 'The percentage of users using Apple\'s machines in the arab world.', '1'),
 (6, 45, 'wow', '2', 'asdasdasdasdasd', '1'),
-(7, 44, 'firstSmart', '7', 'first smart survey', '1');
+(7, 44, 'firstSmart', '7', 'first smart survey', '1'),
+(8, 2, 'efe', '4', 'ecew', '1'),
+(9, 44, 'kjhg', '0', 'mnbv', '1'),
+(10, 44, 'kjhg', '0', 'mnbv', '1'),
+(11, 44, 'something', '2', 'some', '0'),
+(12, 44, 'something', '2', 'some', '0'),
+(15, 44, 'Mohannad', '0', 'Mohannad', '1'),
+(16, 44, 'IssaIss', '0', 'a', '1'),
+(17, 44, 'lkjh', '0', 'lkj', '1'),
+(18, 44, 'kj', '0', ',k', '1'),
+(19, 44, 'cxzxcz', '0', 'xz', '1'),
+(20, 44, 'Mohannad', '0', 'Mohannads', '1'),
+(21, 44, 'Issa', '0', 'Issa', '1'),
+(22, 44, 'bvcxrewq', '0', 'ewq', '1'),
+(23, 44, 'l;kj', '0', 'lkj', '1'),
+(24, 44, 'uiyhg', '0', 'bvfr', '1'),
+(25, 44, 'uhjiu', '0', 'nghg', '1'),
+(26, 45, 'Evgen Feedback', '2', 'What do you think of his technical skills and the freelance experiece he has?', '1'),
+(27, 44, 'test', '0', 'test', '1'),
+(28, 44, 'mnh', '0', 'mkjh', '1'),
+(29, 44, 'test', '0', 'test', '1'),
+(30, 44, 'test', '0', 'test', '1'),
+(31, 70, 'test', '0', 'test', '1'),
+(32, 70, 'test', '0', 'test', '1'),
+(33, 70, 'test', '0', 'test', '1'),
+(34, 70, 'test', '0', 'test', '1'),
+(35, 70, 'Mohannad', '0', 'mO', '1'),
+(36, 70, 'Mohannad', '0', 'Mo', '1');
 
 -- --------------------------------------------------------
 
@@ -550,24 +603,10 @@ INSERT INTO `users` (`id`, `username`, `firstname`, `midname`, `lastname`, `birt
 (2, 'issa', 'issa', 'ali', 'anagrah', '2017-08-15 00:00:00', '1', 'irbid', 'issa@issa.com', '12344321'),
 (6, 'a', 'aa', 'a', 'aaa', '2017-08-15 00:00:00', '1', 'a', 'a.@a.com', '$2a$10$TKFWBsxW0GOfwJcunhrXMuhnJ//f9iA0rUUP8byNjCPG.FjsWb1Rq'),
 (9, 'aa', 'aa', 'a', 'aaa', '2017-08-15 00:00:00', '1', 'a', 'aa.@aa.com', '$2a$10$zhHjc9jAGZfz7MBAUqSDr.iHVpV7zCeUrBAcb/y8ro74Upc/dfFne'),
-(16, 'b', 'b', 'b', 'b', '1980-01-01 00:00:00', '0', 'Amman', 'b@b.com', '$2a$10$CMPs0I3XDLhWg5cQlofng.aTdyyGiIXbu0D6PUpSsQ.g4./NTuWDa'),
-(22, 'ee', 'ee', 'ee', 'ee', '1980-01-01 00:00:00', '0', 'Amman', 'ee@ee.com', '$2a$10$97rBwsDbutXOu7IvIhhOGO3gzzJ.UfoeMjhLn2CeVV6e4fmYuKhem'),
-(23, 'qq', 'qq', 'qq', 'qq', '1980-01-01 00:00:00', '0', 'Amman', 'qq@aa.com', '$2a$10$5uERs9yTMvB2R22O5d5CxehebHH38/G75KdJT3gP6QSaujqIKvzBC'),
-(32, 'rr', 'rr', 'rr', 'rr', '1980-01-01 00:00:00', '0', 'Amman', 'isa.anagreh@gmail.com', '$2a$10$T93tutMn0CR/hUQRPpdVR.Xy0kQvb0oEkr9B1kBNHkjGrfuOWUPza'),
-(33, 'm', 'm', 'm', 'm', '1980-01-01 00:00:00', '0', 'Amman', 'm@m.com', '$2a$10$vIfRiApxfaCIsG2VAKLmgOTdf7EzQh8jzM9c9S2i14wiaT9VgLg/m'),
-(34, 'bb', 'bb', 'bb', 'bb', '1980-01-01 00:00:00', '0', 'Amman', 'bb', '$2a$10$3BezpjvDUuYjO9dgl3wz9uMPTuyJiYIs/b7FIJLX8d1GuIETSwgpS'),
-(35, '', '', '', '', '1980-01-01 00:00:00', '0', 'Amman', '', '$2a$10$nsKH39/nvadEWrmV0RI9/uiLVDGCnZrbIQlRAHrW9G0RjIRuC.DXa'),
-(37, 'll', 'kl', 'll', 'll', '1980-01-01 00:00:00', '0', 'Amman', 'll@ll.com', '$2a$10$jG3EB.4nDzLemKU0ODtaNOA9FQGIXxc7S6Y14IglRnOe2d5IT9/lW'),
-(39, 'ert', 'erter', 'tert', 'tert', '1980-01-01 00:00:00', '0', 'Amman', 'tert@erge.com', '$2a$10$oKB1S8RJ7oinWh0iGuDYk.YwTRM.Patuj1EnaooVW0l3CfKixSp4q'),
-(40, 'ertert', 'ertertr', 'tert', 'ertert', '1980-01-01 00:00:00', '0', 'Amman', 'ertretert@ewr.com', '$2a$10$VWEGBoVHV88HXODFCkOl3Omx8v4dhKTNgKzXiQIFoQLPf8dl2YQdu'),
-(41, 'jytj', 'yjy', 'jtyj', 'jtyj', '1980-01-01 00:00:00', '0', 'Amman', 'jtyj@fwefwef.com', '$2a$10$ZpIB4FFIME9eK6sQTzZcmOs2nFdIgJuB83PtWlRlPy3Mko7tMPGWO'),
-(42, 'mn', 'mn', 'mn', 'mn', '1980-01-01 00:00:00', '0', 'Amman', 'mn@mn.com', '$2a$10$fS9CIp28XiUempgNbs1vseh71sWGh7tw6JBlLPlF3Vu6dHqtusDdW'),
-(43, 'moh', 'moh', 'moh', 'moh', '1980-01-01 00:00:00', '0', 'Amman', 'moh@som.com', '$2a$10$bULJKoX0cfcceQOv8P9aqOSVoodIHaLZTJuekWECsjUjIrea9W6J6'),
 (44, 'read', 'read', 'read', 'read', '1980-01-01 00:00:00', '0', 'Amman', 'read@read.com', '$2a$10$uiorq.Wn5pSDdVkMVJsMEecdC6FrawuebYwR4TjEeqtr1ytubeBa2'),
 (45, 'write', 'write', 'write', 'write', '1980-01-01 00:00:00', '0', 'Amman', 'write@write.com', '$2a$10$lGax4Gr/wO2uprzCQuWz9OSWoqDI9SMVlOpJlZV7JTSpjikTXJfDW'),
-(46, 'rewrite', 'rewrite', 'rewrite', 'rewrite', '1980-01-01 00:00:00', '0', 'Amman', 'rewrite@rewrite.com', '$2a$10$N603anFbH4lywgn8fnJKX.rVIQfXxSV9wufdVvfqUCEKPDWc51F6q'),
-(47, 'reread', 'reread', 'reread', 'reread', '1980-01-01 00:00:00', '0', 'Amman', 'reread', '$2a$10$k2xD1EPf6WejzOoN8c1SFutBB3l6arpLvEiqQnsDkj6HLSA57ORE6'),
-(50, 'qqq', 'qqq', 'qqq', 'qqq', '1980-01-01 00:00:00', '0', 'Amman', 'qqqq@q.com', '$2a$10$WitGh9kkNMEt5TZWc3SlnebJ10ZKFhPaggelfg61KcCzzrE5Hzjq6');
+(70, 'test', 'test', 'test', 'test', '1997-01-16 00:00:00', '0', 'Amman', 'test@test.com', '$2a$10$mTJMfCXVuM6MfDBeg5ggd.F6G64GwvaikXs6u.OobnNB9ifewaxSq'),
+(71, 'aaaa', 'aaa', 'aaa', 'aaa', '1980-01-01 00:00:00', '0', 'Amman', 'aa@aa.com', '$2a$10$saAQgSm8XyiSdlwGPpsSc.exbjkXFdL3rn5nR1MzKs7xI9S.ORA2a');
 
 --
 -- Indexes for dumped tables
@@ -586,6 +625,7 @@ ALTER TABLE `answers`
 -- Indexes for table `choices`
 --
 ALTER TABLE `choices`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `choices_ibfk` (`id_qustion`),
   ADD KEY `choices_ibfk22` (`id_suervey`);
 
@@ -596,6 +636,15 @@ ALTER TABLE `custmoers`
   ADD PRIMARY KEY (`id`),
   ADD KEY `custmoersfk1` (`id_user`),
   ADD KEY `custmoersfk2` (`username`);
+
+--
+-- Indexes for table `dummy`
+--
+ALTER TABLE `dummy`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dummy_ibfk_1` (`id_questions`),
+  ADD KEY `dummy_ibfk_2` (`id_users`),
+  ADD KEY `dummy_ibfk_3` (`id_surveys`);
 
 --
 -- Indexes for table `questions`
@@ -637,37 +686,49 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=209;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=295;
+
+--
+-- AUTO_INCREMENT for table `choices`
+--
+ALTER TABLE `choices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `custmoers`
 --
 ALTER TABLE `custmoers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `dummy`
+--
+ALTER TABLE `dummy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `smart`
 --
 ALTER TABLE `smart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `surveys`
 --
 ALTER TABLE `surveys`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
 
 --
 -- Constraints for dumped tables
@@ -695,6 +756,14 @@ ALTER TABLE `choices`
 ALTER TABLE `custmoers`
   ADD CONSTRAINT `custmoersfk1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `custmoersfk2` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
+
+--
+-- Constraints for table `dummy`
+--
+ALTER TABLE `dummy`
+  ADD CONSTRAINT `dummy_ibfk_1` FOREIGN KEY (`id_questions`) REFERENCES `questions` (`id`),
+  ADD CONSTRAINT `dummy_ibfk_2` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `dummy_ibfk_3` FOREIGN KEY (`id_surveys`) REFERENCES `surveys` (`id`);
 
 --
 -- Constraints for table `questions`
